07a83c71d0a3fbe133689116eb16aec70ac3dae1
Ebtesham-21 <ebteshamhossain98@gmail.com>
2025-01-27 16:22:13 +0600

    layout

--- discountCalculator/app/(tabs)/index.tsx
+++ discountCalculator/app/(tabs)/index.tsx
@@ -1,60 +1,45 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, StyleSheet, Platform, ViewStyle, StyleProp, Text } from 'react-native';
 
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { View, SafeAreaView } from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
 
-export default function HomeScreen() {
+type ContainerStyle = ViewStyle;
+interface HomeScreenProps {
+  containerStyle?: StyleProp<ViewStyle>;
+}
+
+
+
+export default function HomeScreen({containerStyle}: HomeScreenProps) {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <SafeAreaView style={[styles.container, containerStyle]  } >
+      <View style={{justifyContent: 'center', alignItems: 'center', marginTop: 40}}>
+        <Text style={{fontSize: 22}}>Discount Calculator</Text>
+       
+      </View>
+      <View style={{marginHorizontal: 40}}>
+        <Text style={{marginBottom: 10}}>Amount</Text>
+        <TextInput style={{backgroundColor: 'white', paddingVertical: 20, borderRadius:20,
+          borderColor: 'black', borderWidth: 1,
+        }}/>
+      </View>
+      
+
+
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: 'lightblue',
+  },
   titleContainer: {
     flexDirection: 'row',
     alignItems: 'center',
